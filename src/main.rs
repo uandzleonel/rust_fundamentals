75c3717b657115fa043fbff4af08b28f80f42122
Uanderson Leonel <dev.uandz@gmail.com>
2024-04-10 13:28:36 -0300

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod http;
+mod server;
+
+//use http::Method;
+//use http::Request;
+use server::Server;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+    dbg!(server);
 }
